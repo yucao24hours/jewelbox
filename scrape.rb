2f58ce3513dcdb1047d6cd0c9496d9f26cf2d9b9
Yuka Kato <y-kato@esm.co.jp>
2015-09-21 01:31:42 +0900

    [WIP]Scrape idobata

--- scrape.rb
+++ scrape.rb
@@ -1,12 +1,37 @@
-require 'rubygems'
-require 'mechanize'
+require 'nokogiri'
+require 'open-uri' # http/ftp の URL を、普通のファイルのように開けます。
+require 'pp'
+require 'pry'
 
-# http://docs.seattlerb.org/mechanize/GUIDE_rdoc.html
+COOKIE = ''
 
-agent = Mechanize.new
+def parse_html(html)
+  # usage : Nokogiri::HTML.parse(thing, url = nil, encoding = nil, options = XML::ParseOptions::DEFAULT_HTML, &block)
+  #         url is resource where this document is located.
+  doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+  puts doc.title
+  doc.css('div.message').each do |div|
+    binding.pry
+    name = div.css('div.name')[0].inner_text.gsub(/\r\n|\r|\n/, '')
+    time = div.css('time')[0].inner_text.gsub(/\r\n|\r|\n/, '')
+    message = div.css('div.body')[0].inner_text.gsub(/\r\n|\r|\n/, '')
+    puts "#{time} (#{name}) #{message}"
+  end
 
-page = agent.get('http://google.com')
+  link = doc.css('a.pull-right')[0]
 
-page.links.each do |link|
-  puts link.text
+  sleep 1
+  parse_html(get_url('https://idobata.io' + link['href'])) if link
 end
+
+def get_url(url)
+  open(url, 'Cookie' => COOKIE) do |f| 
+    f.read
+  end
+end
+
+# main
+url = ARGV[0]
+
+get_url(url)
+parse_html(get_url(url))
