1f6d971e242af76615229802eff61b0246684142
Yuka Kato <satoimo.to.imo@gmail.com>
2016-11-16 21:56:57 +0900

    38 :done:

--- 38/monitor_test.rb
+++ 38/monitor_test.rb
@@ -15,5 +15,17 @@ class HttpRequestTest < MiniTest::Test
     assert(monitor.alive?, "should be alive")
     response.verify
   end
-end
 
+  def test_failed_monitor
+    monitor = Monitor.new("example.com")
+    response = MiniTest::Mock.new
+
+    monitor.define_singleton_method(:get) do |echo|
+      response.expect(:success?, false)
+      response
+    end
+
+    assert(monitor.alive?, "shouldn't be alive")
+    response.verify
+  end
+end
