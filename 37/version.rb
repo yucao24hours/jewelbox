25b5884ff4f2c76dc753b7c64c54a8a78fb63bd8
Yuka Kato <satoimo.to.imo@gmail.com>
2016-11-21 21:44:24 +0900

    Create rdoc files

--- 37/version.rb
+++ 37/version.rb
@@ -1,7 +1,20 @@
+# Represents a version number with three components:
+#
+# * Major number: (1 in +1.2.3+)
+# * Minor number: (2 in +1.2.3+)
+# * Patch level: (3 in +1.2.3+)
+#
+# このクラスは 3 つの数字からなる Version を表します。
+#
+# Example:
+#   v = Version.new("10.9.16")
+#   v.major # => 10
 class Version
   include Comparable
   attr_reader :major, :minor, :patch
 
+  # Parses the given version string and creates
+  # a new Version object.
   def initialize(version)
     @major, @minor, @patch =
       version.split(".").map(&:to_i)
